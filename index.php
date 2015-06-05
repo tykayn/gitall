cbcb41bac9fc65e20f4de1617cac4df669459315
Baptiste Lemoine <baptiste.lemoine@cofelyineo-gdfsuez.com>
2015-06-05 15:03:58 +0200

    start debug

--- index.php
+++ index.php
@@ -2,14 +2,26 @@
 /**
  * lister les commits de git
  */
+// config
+$file = __DIR__ . '/git-history.txt';
 
 // ouvrir le fichier texte
+$f = fopen($file, "r");
+$content = file_get_contents($file) ;
+//var_dump($file);
+//var_dump($content);
+var_dump(count($lignes));
+
+// couper par ligne
+$lignes = explode('\n', $content);
+// conversion de date
 
 // trier
 
 // output html
 $rep = '<h1>Git log all</h1>';
 $rep .= 'Projet commencé il y a x jours';
+$rep .= '<br/>' .$content ;
 ?>
 
 <html>
