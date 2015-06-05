e23accfd7ed4c8b6e4f3e9319d83427e8226900e
Baptiste Lemoine <baptiste.lemoine@cofelyineo-gdfsuez.com>
2015-06-05 14:55:06 +0200

    hopla c'est parti pour loguer du git

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * lister les commits de git
+ */
+
+// ouvrir le fichier texte
+
+// trier
+
+// output html
+$rep = '<h1>Git log all</h1>';
+$rep .= 'Projet commencé il y a x jours';
+?>
+
+<html>
+<head>
+    <meta charset="UTF-8"></meta>
+    <title>Git log all</title>
+</head>
+<body>
+<?php echo $rep; ?>
+</body>
+</html>
