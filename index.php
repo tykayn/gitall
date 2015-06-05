16aee07a2de2b47baeb461fc366ff93640c45336
Baptiste Lemoine <baptiste.lemoine@cofelyineo-gdfsuez.com>
2015-06-05 15:22:40 +0200

    rangement pour prépaprer au tri par date

--- index.php
+++ index.php
@@ -7,21 +7,55 @@ $file = __DIR__ . '/git-history.txt';
 
 // ouvrir le fichier texte
 $f = fopen($file, "r");
-$content = file_get_contents($file) ;
+$content = file_get_contents($file);
 //var_dump($file);
 //var_dump($content);
-var_dump(count($lignes));
+//var_dump(count($lignes));
 
 // couper par ligne
-$lignes = explode('\n', $content);
+$lignes = explode(';', $content);
+// trier
+$group = [];
+$byDate = [];
+foreach ($lignes as $l) {
+    $boom = explode('/', $l);
+    $date = $boom[0];
+    $auth = $boom[1];
+    $sha = $boom[2];
+    $msg = $boom[3];
 // conversion de date
 
-// trier
+    $timestamp = strtotime($date);
+
+
+    $commit = [
+        'date' => $date,
+        'auth' => $auth,
+        'sha' => $sha,
+        'msg' => $msg,
+    ];
+    $byDate[$timestamp] = $commit;
+}
+// préparer l'affichage
+$display = '';
+foreach ($byDate as $d) {
+
+    //test de tranche différente
+//    var_dump($timestamp);
+//    $an = date('Y', $timestamp);
+//    $mois = date('m', $timestamp);
+//    $jour = date('d', $timestamp);
+//    $heure = date('H', $timestamp);
+
+    $display .= '<br/> '.$d[msg];
+}
+
 
 // output html
 $rep = '<h1>Git log all</h1>';
 $rep .= 'Projet commencé il y a x jours';
-$rep .= '<br/>' .$content ;
+$rep .= '<br/>' . $display;
+$rep .= '<hr/>' . $content;
 ?>
 
 <html>
