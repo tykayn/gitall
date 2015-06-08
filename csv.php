9b693654dbd7d47c59946dfbcdc3e084e26a9e71
gitprep <gitprep@example.com>
2015-06-08 22:14:52 +0200

    fix die

--- csv.php
+++ csv.php
@@ -1,11 +1,10 @@
 <?php
 // rendu en csv
-//var_dump($csv_html);
 $out = __DIR__ . '/output/gitall.csv';
 if( file_exists($out)){
     unlink($out);
     $messages .= '<br/> gitall.csv régénéré';
 }
 
-file_put_contents($out, $csv_html) || die('erreur avec le csv');
+file_put_contents($out, $csv_html) || die('erreur avec le csv');
 chmod($out, 0777);
